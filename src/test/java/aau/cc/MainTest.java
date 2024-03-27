1ac31f196abdcf806ec36290dcb634d3c8a32c94
WilhelmStu <willi.stuhle@gmail.com>
2024-03-27 17:05:13 +0100

    Init

---
+++ src/test/java/aau/cc/MainTest.java
@@ -0,0 +1,26 @@
+package aau.cc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+    void test(){
+        System.out.println("Test works");
+        assertTrue(true);
+    }
+}
